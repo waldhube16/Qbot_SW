8341274e64a7999736b84199ebc24b4d5ed0dea1
waldhube16 <simon.waldhuber@edu.fh-joanneum.at>
2021-01-24 10:46:25 +0100

    Removed old ImageProcessing

--- ImageProcessing/crop.m
+++
@@ -1,9 +0,0 @@
-A = imread('Left.jpeg');
-width = 200;
-height = 200;
-[rows, columns, numberOfColorChannels] = size(A);
-left = columns/2 - width/2;
-top = rows/2 - height/2;
-croppedImage = imcrop(A, [left, top, width, height]);
-imshow(croppedImage)
-
